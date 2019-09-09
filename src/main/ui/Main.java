f87a445f0c567d6bf425da0c3596167701753374
David <davidlcampbell@shaw.ca>
2019-09-09 09:42:02 -0700

    Updated Project File

---
+++ src/main/ui/Main.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Message");
+        main2();
+    }
+
+    public static void main2() {
+        System.out.println("Another Message");
+    }
+}
