c65e1cf6a521c250e8cbf7dd4b22f04a12d0ec98
David <davidlcampbell@shaw.ca>
2019-10-17 09:51:34 -0700

    Checkstyle adjusted: Fixed both tests for the 'remove on empty' exception, both are passing, program runs

--- src/main/model/exceptions/RemoveOnEmptyListException.java
+++ src/main/model/exceptions/RemoveOnEmptyListException.java
@@ -2,6 +2,7 @@ package model.exceptions;
 
 public class RemoveOnEmptyListException extends Exception {
     String newLine = System.getProperty("line.separator");
+
     public RemoveOnEmptyListException() {
         System.out.println("ERROR: No items in the list to remove, please add an item: " + newLine);
     }
